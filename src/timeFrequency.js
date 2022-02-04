0d4f71fa3e6411ccecf67bda88f2971e52db3eea
bruno <bweisz@dc.uba.ar>
2022-02-04 14:39:00 -0300

    fixed frequency heightmap, started frequency time

--- src/timeFrequency.js
+++ src/timeFrequency.js
@@ -1,12 +1,12 @@
 import * as THREE from 'https://cdn.skypack.dev/three';
 
-function drawHeightmapFrequency(analyser){
-    drawHeightmapFrequencyWithSize(analyser, 64, 64);
+function drawTimeFrequency(analyser){
+    drawTimeFrequencyWithWidth(analyser, 64);
 }
 
-function drawHeightmapFrequencyWithSize(analyser, hor, ver){
+function drawTimeFrequencyWithWidth(analyser, wid){
 
-    console.log("started heightmap frequency");
+    console.log("started time frequency");
     let fov = 75;
     let ratio = canvas.clientWidth / canvas.clientHeight;
     let near = .1;
@@ -21,38 +21,49 @@ function drawHeightmapFrequencyWithSize(analyser, hor, ver){
     scene.add(light);
     light.position.set(0,10,0);
     light.target.position.set(0,0,0);
-    camera.position.set(0,20,45);
+    camera.position.set(0,20,10);
     camera.lookAt(0,0,0);
+
+    let advanced = 0;
+    let pace = 0.1;
 
     let vertices = [];
     let colors = [];
     let index = [];
 
-    for(let i = -hor/2; i < hor/2; i++ ){
-        for(let j = -ver/2; j < ver/2; j++ ){
-            vertices.push([i,0,j]);
-            colors.push([0.2 + i/(hor*0.8), 0, 0.2 + j/(ver*0.8)]);
-            // colors.push([1,1,1]);
+    let verticesInitial = [];
+    let colorsInitial = [];
+    for (let i = 0; i < wid; i++){
+        verticesInitial.push([i,0,advanced]);
+        colorsInitial.push([.1,.1,i/wid]);
+    }
+    vertices.push(verticesInitial);
+    colors.push(colorsInitial);
+    advanced += pace;
+
+    let verticesInitial2 = [];
+    let colorsInitial2 = [];
+    for (let i = 0; i < wid; i++){
+        verticesInitial.push([i,0,advanced]);
+        colorsInitial2.push([.1,.1,i/wid]);
+    }
+    vertices.push(verticesInitial2);
+    colors.push(colorsInitial2);
+
+    for(let i = 0; i < vertices.length - 1; i++){
+        for(let j = 0; j < hor-1; j++){
+            index.push(i*hor+j);
+            index.push(i*hor+(j+1));
+            index.push((i+1)*hor+j);
+
+            index.push(i*hor+(j+1));
+            index.push((i+1)*hor+(j+1));
+            index.push((i+1)*hor+j);
         }
     }
-    for(let i = 0; i < hor - 1; i++ ){
-        for(let j = 0; j < ver - 1; j++ ){
-            index.push(i*ver+j);
-            index.push(i*ver+(j+1));
-            index.push((i+1)*ver+j);
 
-            index.push(i*ver+(j+1));
-            index.push((i+1)*ver+(j+1));
-            index.push((i+1)*ver+j);
-
-        }
-    }
-
-    vertices = vertices.flat();
-    colors = colors.flat();
-
-    let floatVertices = new Float32Array(vertices);
-    let floatColors = new Float32Array(colors);
+    let floatVertices = new Float32Array(vertices.flat(2));
+    let floatColors = new Float32Array(colors.flat(2));
 
     let plane = new THREE.BufferGeometry();
     plane.dynamic = true;
@@ -60,24 +71,23 @@ function drawHeightmapFrequencyWithSize(analyser, hor, ver){
     plane.setAttribute('position', new THREE.BufferAttribute(floatVertices, 3));
     plane.setAttribute('color', new THREE.BufferAttribute(floatColors, 3));
     plane.setIndex(index);
-    //plane.computeVertexNormals();
+    plane.computeVertexNormals();
 
-    const basicMaterial = new THREE.MeshPhongMaterial({
+    const phongMaterial = new THREE.MeshPhongMaterial({
         vertexColors: true,
     });
-    const heightmap = new THREE.Mesh(plane, basicMaterial);
+    const heightmap = new THREE.Mesh(plane, phongMaterial);
     heightmap.material.side = THREE.DoubleSide;
 
     scene.add(heightmap);
-    console.log(index);
-    
+    // console.log(index);
 
-    analyser.fftSize = hor*ver*2*2;
+    analyser.fftSize = wid*2;
     let bufferLength = analyser.frequencyBinCount;
     let dataArray = new Uint8Array(bufferLength);
 
     renderer.render(scene,camera);
-    function render(time) {
+    /*function render(time) {
         adaptSize();
         analyser.getByteFrequencyData(dataArray);
 
@@ -85,8 +95,8 @@ function drawHeightmapFrequencyWithSize(analyser, hor, ver){
             for(let j = 0; j < ver; j++ ){
                 const arrayIndex = i*ver+j;
                 const bufferIndex = (i*ver+j)*3+1; 
-                vertices[bufferIndex]/*[1]*/ = computeHeight(dataArray[arrayIndex]);
-                colors[bufferIndex]/*[1]*/ = computeColor(dataArray[arrayIndex]);
+                vertices[bufferIndex] = computeHeight(dataArray[arrayIndex]);
+                colors[bufferIndex] = computeColor(dataArray[arrayIndex]);
                 //heightmap.geometry.setAttribute('position', new THREE.BufferAttribute(new Float32Array(vertices.flat()), 3));
                 //heightmap.geometry.setAttribute('color', new THREE.BufferAttribute(new Float32Array(colors.flat()), 3));
                 heightmap.geometry.attributes.position.array[bufferIndex] = computeHeight(dataArray[arrayIndex]);
@@ -100,7 +110,7 @@ function drawHeightmapFrequencyWithSize(analyser, hor, ver){
         //heightmap.rotation.y = time*0.0005;
         renderer.render( scene, camera );
         animationFrameId = requestAnimationFrame(render);
-    };
+    };*/
     animationFrameId = requestAnimationFrame(render.bind(this, analyser));
 
     function computeHeight(data){
@@ -122,4 +132,4 @@ function drawHeightmapFrequencyWithSize(analyser, hor, ver){
     };
 }
 
-export {drawHeightmapFrequency};
+export {drawTimeFrequency};
